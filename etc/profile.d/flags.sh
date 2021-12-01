11ccb342c31c02d5a377a29a1af2197f1597a978
Mae Dartmann <mae@meanderingmind.me>
2021-12-01 12:27:44 +0100

    Fix typo in LDFLAGS
    Signed-off-by: Mae Dartmann <mae@meanderingmind.me>
    
     Changes to be committed:
    	modified:   etc/profile.d/flags.sh

--- etc/profile.d/flags.sh
+++ etc/profile.d/flags.sh
@@ -9,7 +9,7 @@ export LD=ld.bfd
 
 export CFLAGS='-march=native -mtune=native -O3 -pipe -fno-plt -fexceptions -Wp,-D_FORTIFY_SOURCE=2 -Wformat -Werror=format-security -fstack-clash-protection -fcf-protection'
 export CXXFLAGS="$CFLAGS -Wp,-D_GLIBCXX_ASSERTIONS"
-export LDFLAGS='-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,nowx '
+export LDFLAGS='-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now'
 export RUSTFLAGS="-C opt-level=2"
 export DEBUG_CFLAGS="-g -fvar-tracking-assignments"
 export DEBUG_CXXFLAGS="$DEBUG_CFLAGS"
