aca83f0f4ed874142fb2a8bfe532cd6bd2ee24e7
Mae Dartmann <mae@meanderingmind.me>
2021-12-01 11:18:06 +0100

    Add profile
    Signed-off-by: Mae Dartmann <mae@meanderingmind.me>
    
     Changes to be committed:
    	new file:   profile
    	new file:   profile.d/flags.sh
    	new file:   profile.d/kiss.sh

---
+++ etc/profile.d/flags.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+export CARCH='x86_64'
+export CHOST="$CARCH-pc-linux-gnu"
+
+export CC=gcc
+export CXX=g++
+export LD=ld.bfd
+
+export CFLAGS='-march=native -mtune=native -O3 -pipe -fno-plt -fexceptions -Wp,-D_FORTIFY_SOURCE=2 -Wformat -Werror=format-security -fstack-clash-protection -fcf-protection'
+export CXXFLAGS="$CFLAGS -Wp,-D_GLIBCXX_ASSERTIONS"
+export LDFLAGS='-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,nowx '
+export RUSTFLAGS="-C opt-level=2"
+export DEBUG_CFLAGS="-g -fvar-tracking-assignments"
+export DEBUG_CXXFLAGS="$DEBUG_CFLAGS"
+export DEBUG_RUSTFLAGS="-C debuginfo=2"
+
+export MAKEFLAGS="-j$(nproc)"
---
+++ etc/profile.d/kiss.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+export KISS_GET=curl
+export KISS_COMPRESS=zst
+export KISS_PATH=/var/db/kiss/repo
