a9e8a9570821fcc98ce8280975f1cc33bc449ad5
Mae Dartmann <mae@meanderingmind.me>
2021-12-10 11:58:22 +0100

    Add extra folder to KISS_PATH
    Signed-off-by: Mae Dartmann <mae@meanderingmind.me>
    
     Changes to be committed:
    	modified:   etc/profile.d/kiss.sh

--- etc/profile.d/kiss.sh
+++ etc/profile.d/kiss.sh
@@ -2,4 +2,4 @@
 
 export KISS_GET=curl
 export KISS_COMPRESS=zst
-export KISS_PATH=/var/db/kiss/repo/core:/var/db/kiss/repo/gui
+export KISS_PATH=/var/db/kiss/repo/core:/var/db/kiss/repo/extra:/var/db/kiss/repo/gui
