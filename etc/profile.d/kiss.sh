094fe18bbe3dd20dfdfbdac78a0a7e56c506a3bd
Mae Dartmann <hello@maedartmann.name>
2022-05-05 15:47:32 +0200

    add kiss-hooks system and automatic infodir updater
    Signed-off-by: Mae Dartmann <hello@maedartmann.name>
    
     Changes to be committed:
    	new file:   etc/kiss-hooks/update-infodir
    	modified:   etc/profile.d/kiss.sh

--- etc/profile.d/kiss.sh
+++ etc/profile.d/kiss.sh
@@ -3,3 +3,9 @@
 export KISS_GET=curl
 export KISS_COMPRESS=zst
 export KISS_PATH=/var/db/kiss/repo/core:/var/db/kiss/repo/extra:/var/db/kiss/repo/gui:/var/db/kiss/repo/gnome
+export KISS_SU="sudo"
+
+export KISS_HOOK=""
+for i in /etc/kiss-hooks/*; do
+    export KISS_HOOK="$KISS_HOOK:$i"
+done
