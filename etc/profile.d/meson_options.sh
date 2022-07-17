0c30d95dafa1e23c0604a9a3e14ac13636347b95
Mae Dartmann <hello@maedartmann.name>
2022-07-18 00:38:54 +0200

    add MESON_32_OPTIONS for multilib builds
    Signed-off-by: Mae Dartmann <hello@maedartmann.name>
    
     Changes to be committed:
    	modified:   etc/profile.d/meson_options.sh

--- etc/profile.d/meson_options.sh
+++ etc/profile.d/meson_options.sh
@@ -8,3 +8,12 @@ export MESON_OPTIONS="--prefix /usr
 -D b_lto=true 
 -D b_pie=true "
 
+export MESON_32_OPTIONS="--prefix /usr 
+--libdir lib32
+--libexecdir lib32
+--sbindir bin 
+--auto-features enabled 
+--wrap-mode nodownload 
+-D b_lto=true 
+-D b_pie=true "
+
